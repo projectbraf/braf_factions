4203108bad5dd85d62ab24a207a24dc5bbbb9267
valmojr <val_mo_jr@hotmail.com>
2023-10-18 20:10:35 -0300

    configurado filepaths do _vest

--- addons/vest/w_vest.hpp
+++ addons/vest/w_vest.hpp
@@ -7,19 +7,19 @@ class braf_Bullet_vest_cra : braf_bullet_vest_base {
 	scopeArsenal = 2;
     scopeCurator = 2;
     displayName = "[CRA] Ballistic Vest, CRAPAT";
-    picture = "\braf_factions\braf_factions_characters_vest\Data\BulletVest\ui\Bullet_vest_Crapat_ui.paa";
+    picture = "\braf\braf_factions\addons\vest\Data\BulletVest\ui\Bullet_vest_Crapat_ui.paa";
     hiddenSelections[] = {
         "camo"
     };
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\Data\BulletVest\Crapat_BulletVest_co.paa"
+        "\braf\braf_factions\addons\vest\Data\BulletVest\Crapat_BulletVest_co.paa"
     };
     class ItemInfo : braf_bullet_vest_itemInfo {
         hiddenSelections[] = {
             "camo"
         };
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\Data\BulletVest\Crapat_BulletVest_co.paa"
+            "\braf\braf_factions\addons\vest\Data\BulletVest\Crapat_BulletVest_co.paa"
         };
     };
 };
@@ -28,33 +28,33 @@ class braf_Bullet_vest_cra : braf_bullet_vest_base {
 class braf_Bullet_vest_green : braf_Bullet_vest_cra {
     displayName = "Ballistic Vest, Green";
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\Data\BulletVest\verde_BulletVest_co.paa"
+        "\braf\braf_factions\addons\vest\Data\BulletVest\verde_BulletVest_co.paa"
     };
     class ItemInfo : braf_bullet_vest_itemInfo {
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\Data\BulletVest\verde_BulletVest_co.paa"
+            "\braf\braf_factions\addons\vest\Data\BulletVest\verde_BulletVest_co.paa"
         };
     };
 };
 class braf_Bullet_vest_blue : braf_Bullet_vest_cra {
     displayName = "Ballistic Vest, Blue";
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\Data\BulletVest\azul_BulletVest_co.paa"
+        "\braf\braf_factions\addons\vest\Data\BulletVest\azul_BulletVest_co.paa"
     };
     class ItemInfo : braf_bullet_vest_itemInfo {
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\Data\BulletVest\azul_BulletVest_co.paa"
+            "\braf\braf_factions\addons\vest\Data\BulletVest\azul_BulletVest_co.paa"
         };
     };
 };
 class braf_Bullet_vest_wdl : braf_Bullet_vest_cra {
     displayName = "Ballistic Vest, Woodland";
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\Data\BulletVest\wdl_BulletVest_co.paa"
+        "\braf\braf_factions\addons\vest\Data\BulletVest\wdl_BulletVest_co.paa"
     };
     class ItemInfo : braf_bullet_vest_itemInfo {
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\Data\BulletVest\wdl_BulletVest_co.paa"
+            "\braf\braf_factions\addons\vest\Data\BulletVest\wdl_BulletVest_co.paa"
         };
     };
 };
@@ -68,13 +68,13 @@ class braf_ephod_vest : braf_ephod_vest_base {
 	scopeArsenal = 2;
     scopeCurator = 2;
     displayName = "[CRA] Ephod Vest";
-    picture = "\braf_factions\braf_factions_characters_vest\Data\ephod\ui\ephod_vest_ui.paa";
+    picture = "\braf\braf_factions\addons\vest\Data\ephod\ui\ephod_vest_ui.paa";
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\Data\ephod\Camo_co.paa"
+        "\braf\braf_factions\addons\vest\Data\ephod\Camo_co.paa"
     };
     class ItemInfo : braf_ephod_vest_itemInfo {
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\Data\ephod\Camo_co.paa"
+            "\braf\braf_factions\addons\vest\Data\ephod\Camo_co.paa"
         };
     };
 };
@@ -87,11 +87,11 @@ class braf_ephod_vest_flag : braf_ephod_vest_flag_base {
     scopeCurator = 2;
     displayName = "[CRA] Ephod Vest (Flag)";
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\Data\ephod\Camo_co.paa"
+        "\braf\braf_factions\addons\vest\Data\ephod\Camo_co.paa"
     };
     class ItemInfo : braf_ephod_vest_flag_ItemInfo {
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\Data\ephod\Camo_co.paa"
+            "\braf\braf_factions\addons\vest\Data\ephod\Camo_co.paa"
         };
     };
 };
@@ -104,15 +104,15 @@ class braf_ephod_balistic_cra : braf_bullet_ephod_vest_base {
 	scopeArsenal = 2;
     scopeCurator = 2;
     displayName = "[CRA] Ballistic Vest, CRAPAT (Ephod)";
-    picture = "\braf_factions\braf_factions_characters_vest\Data\BulletVest\ui\ephod_bullet_ui.paa";
+    picture = "\braf\braf_factions\addons\vest\Data\BulletVest\ui\ephod_bullet_ui.paa";
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\Data\ephod\Camo_co.paa",
-        "\braf_factions\braf_factions_characters_vest\Data\BulletVest\Crapat_BulletVest_co.paa"
+        "\braf\braf_factions\addons\vest\Data\ephod\Camo_co.paa",
+        "\braf\braf_factions\addons\vest\Data\BulletVest\Crapat_BulletVest_co.paa"
     };
     class ItemInfo : braf_bullet_ephod_vest_itemInfo {
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\Data\ephod\Camo_co.paa",
-            "\braf_factions\braf_factions_characters_vest\Data\BulletVest\Crapat_BulletVest_co.paa"
+            "\braf\braf_factions\addons\vest\Data\ephod\Camo_co.paa",
+            "\braf\braf_factions\addons\vest\Data\BulletVest\Crapat_BulletVest_co.paa"
         };
     };
 };
@@ -123,14 +123,14 @@ class braf_ephod_balistic_cra_flag : braf_bullet_ephod_vest_flag_base {
 	scopeArsenal = 2;
     scopeCurator = 2;
     displayName = "[CRA] Ballistic Vest, CRAPAT (Ephod, Flag)";
-    picture = "\braf_factions\braf_factions_characters_vest\Data\BulletVest\ui\ephod_bullet_ui.paa";
+    picture = "\braf\braf_factions\addons\vest\Data\BulletVest\ui\ephod_bullet_ui.paa";
     hiddenSelections[] = {
         "camo",
         "camo1"
     };
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\Data\ephod\Camo_co.paa",
-        "\braf_factions\braf_factions_characters_vest\Data\BulletVest\Crapat_BulletVest_co.paa"
+        "\braf\braf_factions\addons\vest\Data\ephod\Camo_co.paa",
+        "\braf\braf_factions\addons\vest\Data\BulletVest\Crapat_BulletVest_co.paa"
     };
     class ItemInfo : braf_bullet_ephod_vest_flag_itemInfo {
         hiddenSelections[] = {
@@ -138,8 +138,8 @@ class braf_ephod_balistic_cra_flag : braf_bullet_ephod_vest_flag_base {
             "camo1"
         };
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\Data\ephod\Camo_co.paa",
-            "\braf_factions\braf_factions_characters_vest\Data\BulletVest\Crapat_BulletVest_co.paa"
+            "\braf\braf_factions\addons\vest\Data\ephod\Camo_co.paa",
+            "\braf\braf_factions\addons\vest\Data\BulletVest\Crapat_BulletVest_co.paa"
         };
     };
 };
@@ -147,29 +147,29 @@ class braf_ephod_balistic_cra_flag : braf_bullet_ephod_vest_flag_base {
 
 class braf_ephod_balistic_black : braf_ephod_balistic_cra {
     displayName = "Ballistic Vest, Black (Ephod)";
-    picture = "\braf_factions\braf_factions_characters_vest\Data\BulletVest\ui\ephod_bullet_ui.paa";
+    picture = "\braf\braf_factions\addons\vest\Data\BulletVest\ui\ephod_bullet_ui.paa";
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\Data\ephod\Camo_co.paa",
-        "\braf_factions\braf_factions_characters_vest\Data\BulletVest\preto_BulletVest_co.paa"
+        "\braf\braf_factions\addons\vest\Data\ephod\Camo_co.paa",
+        "\braf\braf_factions\addons\vest\Data\BulletVest\preto_BulletVest_co.paa"
     };
     class ItemInfo : braf_bullet_ephod_vest_itemInfo {
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\Data\ephod\Camo_co.paa",
-            "\braf_factions\braf_factions_characters_vest\Data\BulletVest\preto_BulletVest_co.paa"
+            "\braf\braf_factions\addons\vest\Data\ephod\Camo_co.paa",
+            "\braf\braf_factions\addons\vest\Data\BulletVest\preto_BulletVest_co.paa"
         };
     };
 };
 class braf_ephod_balistic_olive : braf_ephod_balistic_cra {
     displayName = "Ballistic Vest, Olive (Ephod)";
-    picture = "\braf_factions\braf_factions_characters_vest\Data\BulletVest\ui\ephod_bullet_ui.paa";
+    picture = "\braf\braf_factions\addons\vest\Data\BulletVest\ui\ephod_bullet_ui.paa";
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\Data\ephod\Camo_co.paa",
-        "\braf_factions\braf_factions_characters_vest\Data\BulletVest\verde_BulletVest_co.paa"
+        "\braf\braf_factions\addons\vest\Data\ephod\Camo_co.paa",
+        "\braf\braf_factions\addons\vest\Data\BulletVest\verde_BulletVest_co.paa"
     };
     class ItemInfo : braf_bullet_ephod_vest_itemInfo {
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\Data\ephod\Camo_co.paa",
-            "\braf_factions\braf_factions_characters_vest\Data\BulletVest\verde_BulletVest_co.paa"
+            "\braf\braf_factions\addons\vest\Data\ephod\Camo_co.paa",
+            "\braf\braf_factions\addons\vest\Data\BulletVest\verde_BulletVest_co.paa"
         };
     };
 };
@@ -183,14 +183,14 @@ class braf_LC2_vest_black : braf_bullet_vest_lc2_base {
 	scopeArsenal = 2;
     scopeCurator = 2;
     displayName = "[EB] Ballistic Vest, Black (LC-2)";
-    picture = "\braf_factions\braf_factions_characters_vest\Data\BulletVest\ui\Bullet_vest_eb_ui.paa";
+    picture = "\braf\braf_factions\addons\vest\Data\BulletVest\ui\Bullet_vest_eb_ui.paa";
     hiddenSelectionsTextures[] = {
-        "\BRAF_Factions\braf_factions_characters_vest\Data\BulletVest\preto_BulletVest_co.paa"
+        "\braf\braf_factions\addons\vest\Data\BulletVest\preto_BulletVest_co.paa"
     };
     descriptionShort = "Ballistic Vest";
     class ItemInfo : braf_bullet_vest_lc2_itemInfo {
         hiddenSelectionsTextures[] = {
-            "\BRAF_Factions\braf_factions_characters_vest\Data\BulletVest\preto_BulletVest_co.paa"
+            "\braf\braf_factions\addons\vest\Data\BulletVest\preto_BulletVest_co.paa"
         };
     };
 };
@@ -202,15 +202,15 @@ class braf_lbv_hiaf : braf_lbv_woodland {
     scope = 2;
     displayName = "[HIAF] LBV, Army Lizard";
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\data\lbv\hiaf\lbv_hiaf_co.paa",
+        "\braf\braf_factions\addons\vest\data\lbv\hiaf\lbv_hiaf_co.paa",
         "",
-        "\braf_factions\braf_factions_characters_vest\Data\lc2\lc2_belt_co.paa"
+        "\braf\braf_factions\addons\vest\Data\lc2\lc2_belt_co.paa"
     };
     class ItemInfo : braf_lbv_item {
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\data\lbv\hiaf\lbv_hiaf_co.paa",
+            "\braf\braf_factions\addons\vest\data\lbv\hiaf\lbv_hiaf_co.paa",
             "",
-            "\braf_factions\braf_factions_characters_vest\Data\lc2\lc2_belt_co.paa"
+            "\braf\braf_factions\addons\vest\Data\lc2\lc2_belt_co.paa"
         };
     };
 };
@@ -218,15 +218,15 @@ class braf_lbv_hiaf_marine : braf_lbv_woodland {
     scope = 2;
     displayName = "[HIAF] LBV, Marine Lizard";
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\data\lbv\hiaf\lbv_hiafm_co.paa",
+        "\braf\braf_factions\addons\vest\data\lbv\hiaf\lbv_hiafm_co.paa",
         "",
-        "\braf_factions\braf_factions_characters_vest\Data\lc2\lc2_belt_co.paa"
+        "\braf\braf_factions\addons\vest\Data\lc2\lc2_belt_co.paa"
     };
     class ItemInfo : braf_lbv_item {
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\data\lbv\hiaf\lbv_hiafm_co.paa",
+            "\braf\braf_factions\addons\vest\data\lbv\hiaf\lbv_hiafm_co.paa",
             "",
-            "\braf_factions\braf_factions_characters_vest\Data\lc2\lc2_belt_co.paa"
+            "\braf\braf_factions\addons\vest\Data\lc2\lc2_belt_co.paa"
         };
     };
 };
@@ -234,15 +234,15 @@ class braf_lbv_cratenia : braf_lbv_woodland {
     scope = 2;
     displayName = "[CRA] LBV, Army Lizard";
     hiddenSelectionsTextures[] = {
-        "\braf_factions\braf_factions_characters_vest\data\lbv\cra\lbv_cra_co.paa",
+        "\braf\braf_factions\addons\vest\data\lbv\cra\lbv_cra_co.paa",
         "",
-        "\braf_factions\braf_factions_characters_vest\Data\lc2\lc2_belt_co.paa"
+        "\braf\braf_factions\addons\vest\Data\lc2\lc2_belt_co.paa"
     };
     class ItemInfo : braf_lbv_item {
         hiddenSelectionsTextures[] = {
-            "\braf_factions\braf_factions_characters_vest\data\lbv\cra\lbv_cra_co.paa",
+            "\braf\braf_factions\addons\vest\data\lbv\cra\lbv_cra_co.paa",
             "",
-            "\braf_factions\braf_factions_characters_vest\Data\lc2\lc2_belt_co.paa"
+            "\braf\braf_factions\addons\vest\Data\lc2\lc2_belt_co.paa"
         };
     };
 };
