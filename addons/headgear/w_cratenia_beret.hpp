355daba5d11a7da259dfb2d5a9ccd6c493eee1ba
Bear <bernardos2002@gmail.com>
2024-10-22 16:38:29 -0300

    adjusted wrong path accidentally re-added when commiting

--- addons/headgear/w_cratenia_beret.hpp
+++ addons/headgear/w_cratenia_beret.hpp
@@ -1,11 +1,12 @@
 //--------------------------Beret----------------------------
 //----- CRATENIA -----
-class braf_cratenia_beret_itemInfo : braf_hiaf_beret_itemInfo {
+class braf_cratenia_beret_itemInfo : braf_hiaf_beret_itemInfo  {
 	uniformModel ="\A3\Characters_F\Common\headgear_beret01.p3d";
     hiddenSelections[] = {"camo"};
     hiddenSelectionsTextures[] = { "\braf\braf_factions\addons\headgear\data\Beret\cratenia\cratenia_regular_co.paa"};
 };
-class braf_cratenia_army_beret : braf_hiaf_beret_base {
+class H_Beret_blk;
+class braf_cratenia_army_beret : H_Beret_blk {
     scope = 2;
     scopeArsenal = 2;
     scopeCurator = 2; 
